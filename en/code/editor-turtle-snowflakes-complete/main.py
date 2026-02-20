28791a378341c13f05682aa7937d22dbe4e9f757
awaywiththespoons <becca.rose@raspberrypi.org>
2026-02-20 10:55:40 +0000

    QA edit

--- en/code/editor-turtle-snowflakes-complete/main.py
+++ en/code/editor-turtle-snowflakes-complete/main.py
@@ -1,29 +1,30 @@
 import turtle
 import random
 
-elsa = turtle.Turtle()
+my_turtle = turtle.Turtle()
 turtle.Screen().bgcolor("grey")
+turtle.speed(1)
 colours = ["cyan", "purple", "white", "blue"]
 
 
-elsa.penup()
-elsa.forward(90)
-elsa.left(45)
-elsa.pendown()
+my_turtle.penup()
+my_turtle.forward(90)
+my_turtle.left(45)
+my_turtle.pendown()
 
 def branch():
-    elsa.color(random.choice(colours))
+    my_turtle.color(random.choice(colours))
     for i in range(3):
         for i in range(3):
-            elsa.forward(30)
-            elsa.backward(30)
-            elsa.right(45)
-        elsa.left(90)
-        elsa.backward(30)
-        elsa.left(45)
-    elsa.right(90)
-    elsa.forward(90)
+            my_turtle.forward(30)
+            my_turtle.backward(30)
+            my_turtle.right(45)
+        my_turtle.left(90)
+        my_turtle.backward(30)
+        my_turtle.left(45)
+    my_turtle.right(90)
+    my_turtle.forward(90)
 
 for i in range(8):
     branch()
-    elsa.left(45)
+    my_turtle.left(45)
